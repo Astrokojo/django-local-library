f2aceca6bccaeb09d7dd66b177859d6406bc5c9c
K.A. Osei-Sarfo <astrokojo@gmail.com>
2024-10-20 01:24:16 +0000

    feat: Author List/Detail View:   - Created the author detail and list views.   - Made these views available at the URLs /catalog/authors/ and /catalog/author/<id>.   - Implemented the necessary URL mappers and views similar to the Book list and detail views.   - Developed new templates for the author views, ensuring they share similar behavior with the book templates.

--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -4,4 +4,10 @@ from . import views
 
 urlpatterns = [
    path('', views.index, name='index'),
+   path('books/',  views.BookListView.as_view(), name ='books'), #view is implemented as a class. This is the recommended way to implement views in Django.
+   path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'), # the pattern for this URL is useful for displaying detailed information about a particular book.
+   path('authors/', views.AuthorListView.as_view(), name='authors'),
+   path('author/<int:pk>', views.AuthorDetailView.as_view(), name='author-detail'),
+   
+   # Warning: The generic class-based detail view expects to be passed a parameter named pk. If you're writing your own function view you can use whatever parameter name you like, or indeed pass the information in an unnamed argument.
 ]
--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views import generic
 
 from .models import Book, Author, BookInstance, Genre
 
+# view written as a function
 def index(request):
     """View function for home page of site."""
 
@@ -15,9 +17,9 @@ def index(request):
 
     # The 'all()' is implied by default.
     num_authors = Author.objects.count()
-    
+
     num_genres = Genre.objects.all().count()
-    
+
 
     context = {
         'num_books': num_books,
@@ -27,8 +29,55 @@ def index(request):
         'num_genres': num_genres,
         'books_titles_containing': books_titles_containing,
     }
-    
-    
+
+
 
     # Render the HTML template index.html with the data in the context variable
     return render(request, 'index.html', context=context)
+
+# view written as a class
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 10
+    context_object_name = 'book_list'   # template variable that will represent the list of books in the template file book_list.html
+
+    # Get 5 books containing the word'the' in the title
+    # queryset = Book.objects.filter(title__icontains='the')[:5]
+
+    #  A more flexible way to filter the books is to override the get_queryset(). No real benefit tho.
+    def get_queryset(self):
+        return Book.objects.filter(title__icontains='a')[:5]
+
+    template_name = 'book_list.html'  # the template name/location
+
+class BookDetailView(generic.DetailView):
+    model = Book
+    
+# a class based view as a function if you don't want tp use the generic class-based view
+# Version 1
+# def book_detail_view(request, primary_key):
+#     try:
+#         book = Book.objects.get(pk=primary_key)
+#     except Book.DoesNotExist:
+#         raise Http404('Book does not exist')
+
+#     return render(request, 'catalog/book_detail.html', context={'book': book})
+
+# Version 2
+    def book_detail_view(request, primary_key):
+        book = get_object_or_404(Book, pk=primary_key)
+        return render(request, 'catalog/book_detail.html', context={'book': book})
+
+class AuthorListView(generic.ListView):
+    model = Author
+    paginate_10 = 10
+    context_object_name = 'author_list'
+    queryset = Author.objects.all().order_by('last_name')
+    template_name = 'author_list.html'
+    
+class AuthorDetailView(generic.DetailView):
+    model = Author
+    
+    def author_detail_view(request, primary_key):
+        author = get_object_or_404(Author, pk=primary_key)
+        return render(request, 'catalog/author_detail.html', context={'author': author})
